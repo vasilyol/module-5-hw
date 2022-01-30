f77c4c74c887a209f7ea5d68fac90ed8ac35b8e1
Vasily Olkhovikov <vasily@propertyvista.com>
2022-01-30 12:48:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { places } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [place, setPlace] = useState(0);
+    const [selectedPlaces, setSelected] = useState([]);
+    const {id, placeName, image} = places[place];
+
+    const previousPlace = () => {
+        setPlace(place => {
+            place--;
+            if (place < 0) {
+                place = places.length -1;
+            }
+            return place;
+        });    
+    }
+
+    const nextPlace = () => {
+        setPlace(place => {
+            place++;
+            if (place > places.length - 1) {
+                place = 0;
+            }
+            return place;
+        });
+    }
+
+    const addPlace = (id) => {
+        const place = places.find(place => place.id === id);
+        if (!selectedPlaces.includes(place)) {
+            const newList = [...selectedPlaces];
+            newList.unshift(place);
+            setSelected(newList);
+        }
+    }
+
+    const removePlace = (id) => {
+        const newPlaces = selectedPlaces.filter(place => place.id !== id);
+        setSelected(newPlaces);
+    }
+
+    const setShowMore = (id) => {
+        const newPlaces = [];
+        selectedPlaces.forEach(place => {
+            if (place.id === id) {
+                const changedPlace = {...place, showMore: !place.showMore};
+                newPlaces.push(changedPlace);
+            } else {
+                newPlaces.push(place);
+            }
+        });
+        setSelected(newPlaces); 
+    }
+
+    return (
+        <div>
+            <div className="main">
+                <div className="container">
+                    <h1>Places to visit</h1>
+                </div>
+                <div className="container">
+                    <img src={image} width="600px" alt="place"/>
+                </div>
+                <div className="container">
+                    <h2>{placeName}</h2>
+                </div>
+                <div className="btn container">
+                    <button onClick={previousPlace}>Prev</button>
+                    <button onClick={() => addPlace(id)}>Add to list</button>
+                    <button onClick={() => setSelected([])}>Remove All</button>
+                    <button onClick={nextPlace}>Next</button>
+                </div>
+            </div>
+
+            <div className="main">
+                <div className="container">
+                    <h2 className="choice-header">Your Choice</h2>
+                </div>
+                {selectedPlaces.map(place => {
+                    const { id, placeName, description, image, showMore } = place;
+                    return (
+                        <div key={id}>
+                            <div className="container">
+                                <h3>{placeName}</h3>
+                            </div>
+                            <div className="container">
+                                <p>{showMore ? description : description.substring(0, 220) + " ...."}
+                                <button className="btn-more" onClick={() => setShowMore(id)}>{showMore ? "Show less" : "Show more"}</button>
+                                </p>
+                            </div>
+                            <div className="container">
+                                <img src={image} width="400px" alt="place"/>
+                            </div>
+                            <div className="container">
+                                <button className="btn" onClick={() => removePlace(id)}>Remove</button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const places = [
+    {
+        id: 1,
+        placeName: "Athenes",
+        description: "The heritage of the Classical Era is still evident in the city, represented by ancient monuments, and works of art, the most famous of all being the Parthenon, considered a key landmark of early Western civilization. The city also retains Roman and Byzantine monuments, as well as a smaller number of Ottoman monuments, while its historical urban core features elements of continuity through its millennia of history.",
+        image: "images/athenes.jpg",
+        showMore: false
+    },
+
+    {
+        id: 2,
+        placeName: "Paris",
+        description: "Paris offers the largest concentration of tourist attractions in France, and possibly in Europe. Besides some of the world's most famous musuems, its has a vibrant historic city centre, a beautiful riverscape, an extensive range of historic monuments, including cathedrals, chapels and palaces, plus one of the most famous nightlife scenes in the world.",
+        image: "images/paris.jpg",
+        showMore: false
+    },
+
+    {
+        id: 3,
+        placeName: "Grand Canyon",
+        description: "'Grand' doesn't begin to do this canyon justice. Measuring approximately 277 river miles in length, up to 18 miles in width and a mile deep, this massive chasm in northern Arizona is truly a natural wonder. For 6 million years, the Grand Canyon has expanded with the help of the mighty Colorado River, and for centuries, people from all over the globe have traveled to gaze out over its red and orange grandeur.",
+        image: "images/grand-canyon.jpg",
+        showMore: false
+    },
+    
+    {
+        id: 4,
+        placeName: "Machu Picchu",
+        description: "Located on top of a mountain in the middle of a tropical forest, it is considered the tourist attraction par excellence in Peru. The historic sanctuary is a place that captivates tourists not only for its dazzling architectural beauty, but also for its important historical-cultural legacy, which has led it to be recognized and admired throughout the world.",
+        image: "images/machu-picchu.jpg",
+        showMore: false
+    },
+
+    {
+        id: 5,
+        placeName: "London",
+        description: "One of the world’s most visited cities, England’s buzzing capital is well worth the visit and has so much to offer, from magnificent history and culture to cutting-edge fashion and food. The city is home to an array of famous tourist attractions. The city attracted 20.42 million international visitors in 2018, making it one of the world's most visited in terms of international visits.[1] It welcomed an additional 27.8 million overnighting domestic tourists in 2017,[2] and had 280 million daytrippers in 2015.",
+        image: "images/london.jpg",
+        showMore: false
+    }
+];
